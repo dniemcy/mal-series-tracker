1102410ff756baa11d7b55490bf893890a9773f2
Daniel <danielmniemiec@gmail.com>
2025-01-13 17:33:36 -0500

    Remove code verifier console logs

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -10,14 +10,12 @@ router.get('/mal', (req, res) => {
     req.session.codeVerifier = codeVerifier;
     req.session.save(() => {
         const codeChallenge = generateCodeChallenge(codeVerifier);
-        console.log('Generated code verifier:', codeVerifier);
         const authUrl = new URL('https://myanimelist.net/v1/oauth2/authorize');
         authUrl.searchParams.append('response_type', 'code');
         authUrl.searchParams.append('client_id', config.mal.clientId);
         authUrl.searchParams.append('code_challenge', codeChallenge);
         authUrl.searchParams.append('code_challenge_method', 'plain');
         authUrl.searchParams.append('redirect_uri', config.mal.redirectUri);
-
         res.redirect(authUrl.toString());
     });
 });
@@ -28,14 +26,10 @@ router.get('/mal/callback', async (req, res) => {
         if (!code) {
             return res.redirect('/');
         }
-        console.log('Session in callback:', req.session);
-        console.log('Code verifier from session:', req.session.codeVerifier);
         const codeVerifier = req.session.codeVerifier;
         if (!codeVerifier) {
-            console.error('No code verifier in session');
             return res.redirect('/?error=no_verifier');
         }
-
         const response = await axios.post('https://myanimelist.net/v1/oauth2/token', 
             new URLSearchParams({
                 client_id: config.mal.clientId,
@@ -51,15 +45,12 @@ router.get('/mal/callback', async (req, res) => {
                 }
             }
         );
-
         const accessToken = response.data.access_token;
-        
         const userResponse = await axios.get('https://api.myanimelist.net/v2/users/@me', {
             headers: {
                 'Authorization': `Bearer ${accessToken}`
             }
         });
-
         const userInfo = userResponse.data;
         res.redirect(`/?token=${accessToken}&user=${userInfo.name}&picture=${encodeURIComponent(userInfo.picture)}`);
     } catch (error) {
